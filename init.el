09bb18304214a2f1caf6ea2de4679b011eefe9e2
Kevin McMorrow <kmcm07+github@gmail.com>
2014-08-03 22:21:07 +0100

    Move emacs customizations to separate file

--- init.el
+++ init.el
@@ -1,42 +1,6 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(backup-directory-alist (quote (("." . "~/.saves"))))
- '(column-number-mode t)
- '(css-indent-offset 2)
- '(custom-enabled-themes (quote (tsdh-dark)))
- '(custom-safe-themes (quote ("025354235e98db5e7fd9c1a74622ff53ad31b7bde537d290ff68d85665213d85" "6fe6ab4abe97a4f13533e47ae59fbba7f2919583f9162b440dd06707b01f7794" default)))
- '(display-time-mode nil)
- '(global-hl-line-mode t)
- '(help-at-pt-display-when-idle (quote (flymake-overlay)) nil (help-at-pt))
- '(help-at-pt-timer-delay 0.9)
- '(js2-allow-keywords-as-property-names nil)
- '(js2-basic-offset 2)
- '(line-number-mode t)
- '(save-place t nil (saveplace))
- '(scroll-bar-mode nil)
- '(scss-compile-at-save nil)
- '(show-paren-mode t)
- '(size-indication-mode nil)
- '(text-mode-hook (quote (turn-on-auto-fill text-mode-hook-identify))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight normal :height 113 :width normal))))
- '(flymake-errline ((t (:inherit default :background "magenta" :foreground "gray0"))))
- '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face :foreground "slate gray"))))
- '(font-lock-comment-face ((t (:foreground "slate gray"))))
- '(js2-error ((t (:foreground "red" :underline (:color "red" :style wave)))) t)
- '(js2-external-variable ((t (:background "firebrick" :foreground "orange"))) t)
- '(js2-warning ((t (:underline (:color "navajo white" :style wave)))) t)
- '(mode-line ((t (:background "gray16" :foreground "sky blue" :box nil :family "DejaVu Sans"))))
- '(mode-line-highlight ((t (:background "black" :box (:line-width 1 :color "white smoke")))))
- '(show-paren-match ((t (:background "deep sky blue")))))
+;; move emacs customizations to a separate file
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
 
 ;; size and position
 (set-frame-height (selected-frame) 52) ; desktop
@@ -59,10 +23,6 @@
 	     '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
-(require 'yasnippet)
-(yas-global-mode 1)
-(require 'dropdown-list)
-
 ;; ido-mode
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
@@ -81,6 +41,7 @@
 (load "~/.emacs.d/setup-ruby.el")
 (load "~/.emacs.d/setup-ace-jump-mode.el")
 (load "~/.emacs.d/setup-web-mode.el")
+(load "~/.emacs.d/setup-yasnippet.el")
 ;;(load "~/.emacs.d/setup-smartparens.el")
 
 ;; smartparens
