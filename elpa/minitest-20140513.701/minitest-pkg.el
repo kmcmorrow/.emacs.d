893afda7a4c72ad8d2a2ae8041d15aaf0554c301
Kevin McMorrow <kmcm07+github@gmail.com>
2016-09-25 02:58:31 +0100

    Update packages

--- elpa/minitest-20140513.701/minitest-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "minitest" "20140513.701" "An Emacs mode for ruby minitest files" (quote ((dash "1.0.0"))))
--- elpa/minitest-20150223.1917/minitest-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "minitest" "20150223.1917" "An Emacs mode for ruby minitest files"
-  '((dash "1.0.0"))
-  :url "https://github.com/arthurnn/minitest-emacs")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
