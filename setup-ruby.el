55163addc5e53b732b2b607ae3edd47c25ca9a73
Kevin McMorrow <kmcm07+github@gmail.com>
2016-09-25 02:50:50 +0100

    Add chef files

--- setup-ruby.el
+++ setup-ruby.el
@@ -27,3 +27,4 @@
 (add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.js.erb$" . js2-mode))
 (add-to-list 'auto-mode-alist '(".axlsx$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Cheffile$" . ruby-mode))
